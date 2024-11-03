77b51d23f09ccf7c98462dd0a1223fb5808a5a96
Heitor-Guimaraes <heitor.perci@gmail.com>
2024-11-03 16:43:30 -0300

    mudanças

--- backend/src/repository/conn.js
+++ backend/src/repository/conn.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const database = await mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
 });
 
 console.log('DB!');
